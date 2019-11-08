e537be22edd748d9d228cd06af68d0c04d73c811
Qws <deno@hotmail.nl>
2019-11-08 16:17:11 +0100

    Converted space to tab (ye manually)

--- app/app.js
+++ app/app.js
@@ -10,94 +10,92 @@ const templatesPath = appPath + "\\templates\\";
 const binPath = appPath + "\\bin\\";
 const fileName = "label-template.html";
 let config = {
-    "height":"3cm",
-    "width":"5cm",
-    "type":"pdf"
+	"height":"3cm",
+	"width":"5cm",
+	"type":"pdf"
 }
 let barcode //the human readable barcode.
 
 window.onload = ()=>{
-    let inputBarcode = document.getElementById("input-barcode");
-    inputBarcode.oninput = ()=>{
-        let barcode = inputBarcode.value;
-        JsBarcode("#canvas", barcode, {"width":1.3, "height":35});
-        saveCanvasToSystem();
-    }
+	let inputBarcode = document.getElementById("input-barcode");
+	inputBarcode.oninput = ()=>{
+		let barcode = inputBarcode.value;
+		JsBarcode("#canvas", barcode, {"width":1.3, "height":35});
+		saveCanvasToSystem();
+	}
 }
 
 function createLabel(){
-    let barcodeOption = {"value": barcode}
-    let html = templateManager.createBarCodeHTML(barcodeOption, "file:\\\\\\" + templatesPath + "image.png");
-    
-    
-    console.log("Label path: "+templatesPath);
-    fs.exists(templatesPath, (exist)=>{
-        if (exist === false){
-            fs.mkdirSync(templatesPath);
-        }
-        fs.writeFile(templatesPath + fileName, html, (error)=>{
-            if (!error) {
-                let path = templatesPath + fileName;
-                createPDF(path);//Read template html file and then build a pdf version from it.
-            }
-            else{
-                console.log(error);
-            }
-        });
-    });
-  }
+	let barcodeOption = {"value": barcode}
+	let html = templateManager.createBarCodeHTML(barcodeOption, "file:\\\\\\" + templatesPath + "image.png");
+
+
+	console.log("Label path: "+templatesPath);
+	fs.exists(templatesPath, (exist)=>{
+		if (exist === false){
+			fs.mkdirSync(templatesPath);
+		}
+		fs.writeFile(templatesPath + fileName, html, (error)=>{
+			if (!error) {
+				let path = templatesPath + fileName;
+				createPDF(path);//Read template html file and then build a pdf version from it.
+			}
+			else{
+				console.log(error);
+			}
+		});
+	});
+}
 
 let createPDF = (path)=>{
-    console.log(config);
-    console.log("creating PDF on path: " + path);
-    var html = fs.readFileSync(path, 'utf8');
-    if(config.type === undefined){
-        config.type = ".pdf";
-    }
-    pdf.create(html, config).toFile(binPath +'label.'+config.type, function(err, res) {
-        if (err) return console.log(err);
-        console.log(res); // { filename: '/bin/label.pdf' }
-        openPDF();
-    });
+	console.log(config);
+	console.log("creating PDF on path: " + path);
+	var html = fs.readFileSync(path, 'utf8');
+	if(config.type === undefined){
+		config.type = ".pdf";
+	}
+	pdf.create(html, config).toFile(binPath +'label.'+config.type, function(err, res) {
+		if (err) return console.log(err);
+		console.log(res); // { filename: '/bin/label.pdf' }
+		openPDF();
+	});
 }
 
 document.getElementById("btn-print-barcode").onclick = () => {
-    printer.print(binPath + "label.pdf");
+	printer.print(binPath + "label.pdf");
 }
 document.getElementById("btn-create-pdf").onclick = () => {createLabel();}
 document.getElementById("btn-barcode-generator").onclick = () => {
-  let barcodeArray = [];
-  let c = 0;
-  while (c < 13)
-  {
-    barcodeArray[c] = Math.floor((Math.random() * 9) + 0);
-    c++;
-  }
-  c = 0;
-  barcode = "";
-  while (c < 13)
-  {
-    barcode = barcode + barcodeArray[c].toString();
-    c++;
-  }
-  JsBarcode("#canvas", barcode, {"width":1.3, "height":35});
-  document.getElementById("input-barcode").value = barcode;
-  saveCanvasToSystem();
+	let barcodeArray = [];
+	let c = 0;
+	while (c < 13){
+		barcodeArray[c] = Math.floor((Math.random() * 9) + 0);
+		c++;
+	}
+	c = 0;
+	barcode = "";
+	while (c < 13){
+		barcode = barcode + barcodeArray[c].toString();
+		c++;
+	}
+	JsBarcode("#canvas", barcode, {"width":1.3, "height":35});
+	document.getElementById("input-barcode").value = barcode;
+	saveCanvasToSystem();
 }
 
 let saveCanvasToSystem = ()=>{
-    let canvas = document.getElementById("canvas");
-    var image = canvas.toDataURL("image/png");
-    console.log(image);
-    const correctImg = image.replace("data:image/png;base64,", "");
-    console.log(correctImg);
-    let buff = Buffer.from(correctImg, "base64"); //converts base64 to binary buffer
-    fs.writeFileSync("./templates/image.png", buff);
+	let canvas = document.getElementById("canvas");
+	var image = canvas.toDataURL("image/png");
+	console.log(image);
+	const correctImg = image.replace("data:image/png;base64,", "");
+	console.log(correctImg);
+	let buff = Buffer.from(correctImg, "base64"); //converts base64 to binary buffer
+	fs.writeFileSync("./templates/image.png", buff);
 }
 
 let openPDF = () =>{
-    let path = binPath + "\label.pdf";
-    path = path.replace(/\//g,"\\");
-    let shell = electron.remote.shell;
-    shell.openItem(path);
+	let path = binPath + "\label.pdf";
+	path = path.replace(/\//g,"\\");
+	let shell = electron.remote.shell;
+	shell.openItem(path);
 }
--- main.js
+++ main.js
@@ -2,23 +2,23 @@ const {app, BrowserWindow} = require('electron');
 let contents; //webContents
 
 function createWindow(){
-  let win = new BrowserWindow({
-    "width":800,
-    "height":400,
-    webPreferences:{
-      nodeIntegration: true
-    }
-  });
-  win.loadFile("./app/index.html");
-  contents = win.webContents;
-}
-exports.print = (option = {}, callback = null) => {
-  if(callback === null){
-    contents.print(option);
-  }
-  else{
-    contents.print(option, callback);
-  }
+	let win = new BrowserWindow({
+		"width":800,
+		"height":400,
+		webPreferences:{
+			nodeIntegration: true
+		}
+		});
+		win.loadFile("./app/index.html");
+		contents = win.webContents;
+	}
+	exports.print = (option = {}, callback = null) => {
+	if(callback === null){
+		contents.print(option);
+	}
+	else{
+		contents.print(option, callback);
+	}
 }
 
 console.log("loaded main.js");
